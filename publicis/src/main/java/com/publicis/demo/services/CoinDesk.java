450d2ebe44fd5363439a677d56804ff2136fe9ee
Nitin Chakre <nitin@xecurify.com>
2024-04-28 21:35:34 +0530

    5 changes

--- publicis/src/main/java/com/publicis/demo/services/CoinDesk.java
+++ publicis/src/main/java/com/publicis/demo/services/CoinDesk.java
@@ -23,7 +23,7 @@ public class CoinDesk {
 
     public String getHistoricData() throws UnirestException {
 
-        System.out.println("\nprint new build 4 \n");
+        System.out.println("\nprint new build 5th \n");
         JSONObject response = new JSONObject();
         try {
             String cacheName = "historicData";
